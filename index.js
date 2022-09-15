8985810221c2b02f404c08f0f605842517405eec
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2022-09-15 10:01:17 -0400

    Added debug logging

--- index.js
+++ index.js
@@ -9,20 +9,23 @@ const issueId = core.getInput('issueId');
 const octokit = github.getOctokit(apiToken);
 
 const main = async () => {
+    console.log("Getting issue...");
     const issue = await (await octokit.rest.issues.get(context.repo.owner, context.repo.repo, issueId)).data;
     
     if(issue.data.comments === 0) {
+        console.log("Issue has no comments...");
         // Nothing to process here
         return;
     }
     
     const comments = (await Promise.resolve(octokit.rest.issues.listComments(context.repo.owner, context.repo.repo, issueId))).data;
     
-    console.log(comments.data.length);
+    console.log(`Issue has ${comments.data.length} comment(s)`);
     
     for (const comment of comments.data) {
         console.log(comment.body);
     }
 }
 
+console.log("Calling main");
 main();
