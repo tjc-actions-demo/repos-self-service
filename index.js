7e31ce60147dc20699daf4bbcc1806e312ede9fc
Thomas Cappelletti <thomas.cappelletti@incyclesoftware.com>
2022-09-15 10:33:12 -0400

    Added check for a repo command

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-
+const command = "/repo";
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 // Get Inputs
 const apiToken = core.getInput('apiToken', { required: true });
 const issueNumber = core.getInput('issueNumber', { required: true });
+const repoToken = core.getInput('repoToken', { required: true });
 
-const octokit = github.getOctokit(apiToken);
+const apiOctokit = github.getOctokit(apiToken);
+const repoOctokit = github.getOctokit(repoToken);
 
 const owner = github.context.repo.owner;
 const repo = github.context.repo.repo
 
 const main = async () => {
-    console.log("Getting issue...");
     console.log(`Issue ID: ${issueNumber}`);
     console.log(`Owner: ${owner}`);
     console.log(`Repo: ${repo}`);
 
-    const { data: issue } = await octokit.rest.issues.get({
+    console.log("Getting issue...");
+    const { data: issue } = await apiOctokit.rest.issues.get({
         owner,
         repo,
         issue_number: issueNumber
@@ -29,7 +31,8 @@ const main = async () => {
         return;
     }
 
-    const { data: comments } = await octokit.rest.issues.listComments({
+    console.log("Getting issue comments...");
+    const { data: comments } = await apiOctokit.rest.issues.listComments({
         owner,
         repo,
         issue_number: issueNumber
@@ -37,8 +40,15 @@ const main = async () => {
 
     console.log(`Issue has ${comments.length} comment(s)`);
 
-    for (const comment of comments) {
-        console.log(comment.body);
+    var latestComment = comments[comments.length - 1];
+
+    if(!latestComment.body_text.includes(command)) {
+        repoOctokit.rest.issues.createComment({
+            owner,
+            repor,
+            issue_number: issueNumber,
+            body: "A repo command was not found in your last comment."
+        })
     }
 }
 
